91d3f6d29c3b6983d4bef4a4c309169b0a68c383
Juan V <jv29092002@gmail.com>
2025-07-23 19:13:01 -0500

    Mod pasajero

--- Pasajero.py
+++ Pasajero.py
@@ -1,10 +1,19 @@
 class Pasajero:
     def __init__(self, nombre, id):
-        self.nombre = nombre
-        self.id = id
+        self.__nombre = nombre
+        self.__id = id
 
     def getId(self):
-        return self.id
+        return self.__id
 
     def getNombre(self):
-        return self.nombre
+        return self.__nombre
+
+    def setNombre(self, nuevo_nombre):
+        self.__nombre = nuevo_nombre
+
+    def setId(self, nueva_id):
+        self.__id = nueva_id
+        
+    def toString(self):
+        return f'Nombre:{self.__nombre}. Id: {self.__id}'
